e4155bdc7a6c6edb737e5547c48f295a84aeb1f7
Ronald <106770875+IARonald@users.noreply.github.com>
2024-11-17 04:32:26 +0000

    ...

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -3,7 +3,7 @@ import userMiddleware from "../middlewares/user.middleware"
 import { checkToken, createToken } from "../middlewares/jwt.middleware"
 const router = Router()
 router.get("/list", checkToken, userMiddleware.listUsers)
-router.get("/games/list", checkToken, userMiddleware.listjogos)
+router.get("/games/list", checkToken, userMiddleware.listjogos);
 
 router.post("/sign", userMiddleware.createUser)
 router.post("/", userMiddleware.dadosjogo);
